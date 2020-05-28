2a17215d6f82cf372a45d90fd049c395c80ba42e
kiheyunkim <kiheyunkim@gmail.com>
2020-05-28 12:04:34 +0900

    fix prev workd2

--- MydataProviderBank/src/main/java/com/multicampus/teamProj4/bank/login/controller/LoginController.java
+++ MydataProviderBank/src/main/java/com/multicampus/teamProj4/bank/login/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.multicampus.teamProj4.bank.Exception.RepositoryException;
+import com.multicampus.teamProj4.bank.login.entity.LoginEntity;
 import com.multicampus.teamProj4.bank.login.service.LoginService;
 
 import java.util.HashMap;
@@ -40,9 +41,9 @@ public class LoginController {
 
 	@PostMapping("/loginRequest")
 	@ResponseBody
-	public Map<String, Object> loginRequest(HttpSession session,@RequestBody HashMap<String, Object> params){
-		String id = (String) params.get("id");
-		String password = (String) params.get("password");
+	public Map<String, Object> loginRequest(HttpSession session, LoginEntity params){
+		String id = params.getId();
+		String password = params.getPassword();
 		
 		String identifyStr = null;
 		Map<String, Object> retval = new HashMap<String, Object>();
@@ -72,9 +73,58 @@ public class LoginController {
 	public String getRegister() {
 		return "join";
 	}
+	
 	@GetMapping("/register_individual")
-	public String getIndividualRegister() {
+	public String getIndividualRegister(HttpSession session) {
+		if(!session.getAttribute("agreementList").equals(null)) {
+			session.removeAttribute("agreementList");
+		}
+		Map<String, Object> agreementMap = new HashMap<String, Object>();
+		agreementMap.put("currentPage", 1);
+		session.setAttribute("agreementList", agreementMap);
+		
+		
 		return "join_individual";
+	}
+	
+	@PostMapping("/register_individual")
+	@ResponseBody
+	public Map<String, Object> postIndividualRegister(HttpSession session, @RequestBody HashMap<String, Object> params){
+		Map<String, Object> retval = new HashMap<String, Object>();
+		@SuppressWarnings("unchecked")
+		Map<String, Object> agreementMap = (Map<String, Object>) session.getAttribute("agreementList");
+		if(agreementMap == null) {
+			retval.put("result", "error_Occured");
+		}
+		
+		int sessionCurrentPage = (int) agreementMap.get("currentPage");
+		int currentPage = (int)params.get("currentPage");
+		
+		//세션의 위치와 정적페이지의 위치가 다르면 에러
+		if(sessionCurrentPage != currentPage) {
+			retval.put("result", "error_Occured");
+			return retval;
+		}
+		
+		if(sessionCurrentPage == 1) {
+			boolean serviceAgreement = (boolean) params.get("serviceAgreement");
+			boolean privacyAgreement = (boolean) params.get("privacyAgreement");
+			
+			if(!serviceAgreement) {
+				retval.put("result", "service_not_agreed");
+				return retval;
+			}
+			else if(!privacyAgreement) {
+				retval.put("result", "privacy_no_agreed");
+				return retval;
+			}
+			else {
+				agreementMap.put("auth1", "ok");
+				retval.put("result", "ok");
+			}
+		}		
+		
+		return retval;
 	}
 }
 
